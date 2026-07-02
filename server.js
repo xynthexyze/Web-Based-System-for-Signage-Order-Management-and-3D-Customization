bff42792d3d6b91c79240e7b42b64a8e88b18e0f
jewel <xynthexyze@gmail.com>
2026-07-03 05:40:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve everything in the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
